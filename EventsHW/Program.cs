408f4fcc2978f7e75b0c3375c91599daa23dee3e
Женя Остапенко <jenya2003a@gmail.com>
2024-02-14 15:06:14 +0200

    Добавьте файлы проекта.

---
+++ EventsHW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Channels;
+
+namespace EventsHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
