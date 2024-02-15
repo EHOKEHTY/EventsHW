26a00efb77c0b4e5773c241eb19e7361b7473c11
Женя Остапенко <jenya2003a@gmail.com>
2024-02-15 14:08:39 +0200

    Создал обозреваемый лист

---
+++ EventsHW/WatchList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsHW
+{
+    internal class WatchList<T> : List<T>
+    {
+        public event EventHandler<ChangesInList<T>>? ItemRemoved;
+        public event EventHandler<ChangesInList<T>>? ItemInserted;
+        public event EventHandler<ChangesInList<T>>? ItemAdded;
+
+        private void OnItemAdded(T item)
+        {
+            ItemAdded?.Invoke(this, new ChangesInList<T>(item));
+        }
+        private void OnItemRemoved(T item)
+        {
+            ItemRemoved?.Invoke(this, new ChangesInList<T>(item));
+        }
+        private void OnItemInserted(int index, T item)
+        {
+            ItemInserted?.Invoke(this, new ChangesInList<T>(item, index));
+        }
+
+        public new void Add(T item)
+        {
+            base.Add(item);
+            OnItemAdded(item);
+        }
+        public new void Remove(T item)
+        {
+            base.Remove(item);
+            OnItemRemoved(item);
+        }
+        public new void Insert(int index, T item)
+        {
+            base.Insert(index, item);
+            OnItemInserted(index, item);
+        }
+    }
+}
